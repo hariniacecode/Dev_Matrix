0f7477c50a00fff132702317a4157322a9cf9f8d
hariniacecode <harini140307@gmail.com>
2026-08-20 01:53:09 +0530

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,77 +1,44 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
+
+from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
+from .models import Profile
 from django.core.mail import send_mail
 from django.conf import settings
-
-from .models import Profile
-
-
+# @receiver(post_save,sender=Profile)  
 def createProfile(sender, instance, created, **kwargs):
-
-    print("=== CREATE PROFILE SIGNAL ===")
-
+    print('Profile Signal Triggered')
     if created:
-
-        print("Creating profile...")
-
-        profile = Profile.objects.create(
-            user=instance,
-            username=instance.username,
-            email=instance.email,
-            name=instance.first_name,
+        user= instance
+        profile =Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
         )
-
-        print("Profile created:", profile)
-
-        try:
-
-            print("Trying to send email to:", profile.email)
-
-            send_mail(
-                subject="Welcome to DevSearch",
-                message="We are glad you are here!",
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[profile.email],
-                fail_silently=True,
-            )
-
-            print("Email function completed")
-
-        except Exception as e:
-
-            print("EMAIL ERROR:", e)
+        
+        subject= 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
 
 
-def updateUser(sender, instance, created, **kwargs):
 
-    if not created:
 
-        profile = instance
-        user = profile.user
-
-        user.first_name = profile.name
+def updateUser(sender,instance,created,**kwargs):
+    profile=instance
+    user=profile.user
+    if created==False:
+        user.first_name=profile.name
         user.username = profile.username
-        user.email = profile.email
-
-        user.save(
-            update_fields=[
-                "first_name",
-                "username",
-                "email"
-            ]
-        )
-
+        user.email==profile.email
+        user.save()
 
 def deleteUser(sender, instance, **kwargs):
-
-    try:
-        instance.user.delete()
-    except Exception as e:
-        print("DELETE ERROR:", e)
-
-
-post_save.connect(createProfile, sender=User)
-post_save.connect(updateUser, sender=Profile)
-post_delete.connect(deleteUser, sender=Profile)
-  
+   try:
+     user = instance.user
+     user.delete()
+   except:
+       pass
+   
+post_save.connect(createProfile,sender=User)
+post_save.connect(updateUser,sender=Profile)
+post_delete.connect(deleteUser,sender=Profile)
