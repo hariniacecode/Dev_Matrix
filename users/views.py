7a553ff0145af4255a20bcddc554d742a7be16b4
hariniacecode <harini140307@gmail.com>
2026-08-20 00:45:15 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -93,15 +93,33 @@ def userAccount(request):
 
 @login_required(login_url='login')
 def editAccount(request):
-    profile = request.user.profile
-    form=ProfileForm()
-    if request.method=='POST':
-        form=ProfileForm(request.POST,request.FILES,instance=profile)
+
+    profile, created = Profile.objects.get_or_create(
+        user=request.user,
+        defaults={
+            'username': request.user.username,
+            'email': request.user.email,
+            'name': request.user.first_name,
+        }
+    )
+
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(
+            request.POST,
+            request.FILES,
+            instance=profile
+        )
+
         if form.is_valid():
             form.save()
             return redirect('account')
-    context={'form':form}
-    return render(request,'users/profile_form.html',context)
+
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
+
+ 
 
 
 @login_required(login_url='login')
