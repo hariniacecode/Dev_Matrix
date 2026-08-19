d277f3d76c14d9f382f2d8fc560c936df4d7e26c
hariniacecode <harini140307@gmail.com>
2026-08-20 02:42:14 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -67,7 +67,7 @@ def registerUser(request):
 def profiles(request):
     profiles,search_query=searchProfiles(request)
 
-    custom_range,projects = paginateProfiles(request,profiles,3)
+    custom_range,profiles= paginateProfiles(request,profiles,3)
 
     context={'profiles':profiles,'search_query':search_query , 'custom_range':custom_range}
     return render(request,'users/profiles.html',context) #render means it will look into the templates folder automatically
